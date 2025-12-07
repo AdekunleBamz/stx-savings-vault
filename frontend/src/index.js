2f922b14d93b74601612a3cba85209551f62098b
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-07 22:12:29 +0100

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM fr 'react-dom/client'
-import App fro './App'
+import App fr './App'
 const root ReactDOM.createRoot(document.getEementByI'root'));
 root.rende
   <React.StrictMode>
